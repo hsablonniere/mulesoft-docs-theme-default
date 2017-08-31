27f7dc5479d19dd43f00ddf86fb2d240d009f546
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2017-08-31 17:15:40 +0200

    use swiftype key to enable/disable the swiftype search widget

---
+++ scripts/search.js
@@ -0,0 +1,7 @@
+(function () {
+  'use strict'
+
+  document.querySelector('.search-open').addEventListener('click', function () {
+    _st._widgetManager._defaultInstall._searchContext._resultsDisplay.setVisible(true)
+  })
+})()
